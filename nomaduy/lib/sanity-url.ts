326c27348003a3800f2841c994ab005de8c0b5f5
Camila Szafran <camilaszafran@gmail.com>
2026-04-20 09:21:49 -0300

    fix: fallback to SANITY_PROJECT_ID in sanity-url when NEXT_PUBLIC vars not set

--- nomaduy/lib/sanity-url.ts
+++ nomaduy/lib/sanity-url.ts
@@ -2,8 +2,8 @@ import { createClient } from 'next-sanity'
 import { createImageUrlBuilder } from '@sanity/image-url'
 
 const publicClient = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  projectId: (process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? process.env.SANITY_PROJECT_ID)!,
+  dataset: (process.env.NEXT_PUBLIC_SANITY_DATASET ?? process.env.SANITY_DATASET)!,
   apiVersion: '2024-01-01',
   useCdn: true,
 })
